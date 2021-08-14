61ba3d73330e06ae390cc36b8937232adde57044
David Mwendwa <klatzdavie19@gmail.com>
2021-08-14 12:33:51 +0300

    feat: create button component and _event detail page

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import Link from 'next/link';
+import Button from '../ui/button';
+import DateIcon from '../icons/date-icon'
+import AddressIcon from '../icons/address-icon'
+import ArrowRightIcon from '../icons/arrow-right-icon'
 
 import classes from './event-item.module.css'
 
@@ -18,20 +21,25 @@ function EventItem({ event }) {
 
   return (
     <li className={classes.item}>
-      <img src={'/' + image} alt={title} width="200px" />
+      <img src={'/' + image} alt={title} />
       <div className={classes.content}>
         <div className={classes.summary}>
           <h2>{title}</h2>
         </div>
         <div className={classes.date}>
+          <DateIcon />
           <time>{humanReadableDate}</time>
         </div>
         <div className={classes.address}>
+          <AddressIcon />
           <address>{formattedAddress}</address>
         </div>
       </div>
       <div className={classes.actions}>
-        <Link href={exploreLink}>Explore Events</Link>
+        <Button link={exploreLink}>
+          <span>Explore Event</span>
+          <span className={classes.icon}><ArrowRightIcon /></span>
+        </Button>
       </div>
     </li>
   );
